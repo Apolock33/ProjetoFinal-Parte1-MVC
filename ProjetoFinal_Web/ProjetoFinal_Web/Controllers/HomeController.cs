11ee0da69b5b54ef66ee3dfd710294fb55f38d74
Apolock33 <carlosammgomes@gmail.com>
2022-01-02 16:17:40 -0300

    comit carlos2

--- ProjetoFinal_Web/ProjetoFinal_Web/Controllers/HomeController.cs
+++ ProjetoFinal_Web/ProjetoFinal_Web/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace ProjetoFinal_Web.Controllers
         {
             return View();
         }
+        public IActionResult Locais()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
