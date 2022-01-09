ab86006fccbcb934707474ff70879b6cec02cb29
vcpersike <vcpersike@gmail.com>
2022-01-08 22:48:08 -0300

    teste

--- frontEnd/projeto-recode/src/pages/Tab1.tsx
+++ frontEnd/projeto-recode/src/pages/Tab1.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonItem, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
@@ -16,6 +16,9 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Projeto 2</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <IonItem>
+        <IonButton color="secundary">Primary</IonButton>
+        </IonItem>
         <ExploreContainer name="Tab 1 page" />
       </IonContent>
     </IonPage>
